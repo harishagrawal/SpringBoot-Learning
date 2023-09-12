d8901b80eba86548177beb70f585513ac1f573a6
harishagrawal <harish@zb.io>
2023-09-12 10:09:38 +0000

    Test generated by RoostGPT for test learn-springboot using AI Type Open AI and AI Model gpt-4

--- 2.x/chapter1-1/src/test/java/com/didispace/chapter11/HelloController_index_b1e8101632_Test.java
+++ 2.x/chapter1-1/src/test/java/com/didispace/chapter11/HelloController_index_b1e8101632_Test.java
@@ -1,11 +1,13 @@
 package com.didispace.chapter11;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -17,9 +19,17 @@ public class HelloController_index_b1e8101632_Test {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private HelloController helloController;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(helloController).build();
+    }
+
     @Test
     public void testIndexSuccess() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("Hello World"))
                 .andDo(MockMvcResultHandlers.print())
@@ -28,7 +38,7 @@ public class HelloController_index_b1e8101632_Test {
 
     @Test
     public void testIndexFailure() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("Hello Mars"))
                 .andDo(MockMvcResultHandlers.print())
